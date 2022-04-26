d8e05a4e28ed70865e396db306f24705939ff837
Joanna Hong <heejin.joanna.hong@gmail.com>
2022-04-25 22:19:47 -0500

    added findByUsername

--- src/main/java/com/example/backend_capstone/jpa/security/user/UserRepository.java
+++ src/main/java/com/example/backend_capstone/jpa/security/user/UserRepository.java
@@ -1,10 +1,11 @@
-package com.example.backend_capstone.jpa.user;
+package com.example.backend_capstone.jpa.security.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
     User findByEmail(String email);
 }
 
