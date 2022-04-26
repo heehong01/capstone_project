79b9eb5130352e60afd2c7a463829f319e43bd63
Joanna Hong <heejin.joanna.hong@gmail.com>
2022-04-25 22:27:06 -0500

    implementation of house dto

--- src/main/java/com/example/backend_capstone/jpa/DTO/HouseSelectionDTO.java
+++ src/main/java/com/example/backend_capstone/jpa/DTO/HouseSelectionDTO.java
@@ -1,4 +1,20 @@
 package com.example.backend_capstone.jpa.DTO;
 
 public class HouseSelectionDTO {
+    private Long houseId;
+
+    public HouseSelectionDTO(Long houseId) {
+        System.out.println("COnstructor" + houseId);
+        this.houseId = houseId;
+    }
+
+    public Long getHouseId() {
+        System.out.println("Getter" +houseId);
+        return houseId;
+    }
+
+    public void setHouseId(Long houseId) {
+        this.houseId = houseId;
+    }
+
 }
